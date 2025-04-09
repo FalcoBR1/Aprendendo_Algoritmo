b4416ed8d64aaf9b70e9cde259390f0c011f9a33
Luiz Henrique <luizhfaranha25@gmail.com>
2025-04-09 19:01:17 -0300

    Update and rename somar2numeros.java to Somar2Numeros.java

--- Somar2Numeros.java
+++ Somar2Numeros.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class somar2numeros {
+public class Somar2Numeros {
     public static void main(String[] args){
         Scanner scn = new Scanner(System.in);
         int num1 = 0;
